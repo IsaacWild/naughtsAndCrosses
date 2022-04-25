45c0234f868e2908f9089ba5bb418ed1d8fdc4e4
Isaac <isaacwildsecond@gmail.com>
2022-04-25 17:01:19 +0100

    Added styling and array for gameplay

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //Object factory for the gameboard logic?
 //This works!!
 const gameBoard = (() => {
-    const gameTitle = document.querySelector('.title');
+
+    let gamePlay = ['O','X','','O','X','','','',''];
+
     const gameGrid = document.querySelector('.gameWrapper')
 
     const _render = () => {
@@ -9,15 +11,13 @@ const gameBoard = (() => {
             const gameBox = document.createElement('div');
             const gamePiece = document.createElement('p')
             gameBox.classList.add('gameBox');
+            gameBox.setAttribute('id',`box${i}`);
             gameBox.appendChild(gamePiece);
             gameGrid.appendChild(gameBox);
         }
     }
     _render();
 
-    const changeTitle = () => gameTitle.textContent = 'Test';
-    const sayHi = () => alert('Hello!!'); 
-    const hiLog = () => console.log('This is a test log!')
+    return { gamePlay }
 
-    return { changeTitle, hiLog };
 })();
