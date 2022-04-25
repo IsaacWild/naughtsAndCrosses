069cc4df374826bf6e2d0226207055b1686ef5a3
DirtyReboot <96993283+IsaacWild@users.noreply.github.com>
2022-04-25 14:39:12 +0100

    Added Render as private function in game board

--- script.js
+++ script.js
@@ -2,13 +2,20 @@
 //This works!!
 const gameBoard = (() => {
     const gameTitle = document.querySelector('.title');
+    const gameGrid = document.querySelector('.gameWrapper')
 
-    const render = 'make game board and insert into DOM';
+    const _render = () => {
+        for (let i = 0; i < 9; i++) {
+            const gameBox = document.createElement('div');
+            gameBox.classList.add('gameBox');
+            gameGrid.appendChild(gameBox);
+        }
+    }
+    _render();
 
     const changeTitle = () => gameTitle.textContent = 'Test';
     const sayHi = () => alert('Hello!!'); 
     const hiLog = () => console.log('This is a test log!')
-
 
     return { changeTitle, hiLog };
 })();
