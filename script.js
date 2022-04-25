5918695024e2a32cb92018b7629f0712f693e2ca
DirtyReboot <96993283+IsaacWild@users.noreply.github.com>
2022-04-25 12:30:14 +0100

    Testing Module 2

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 //Object factory for the gameboard logic?
-
-const gameTitle = document.querySelector('.title');
-
+//This works!!
 const gameBoard = (() => {
+    const gameTitle = document.querySelector('.title');
     const changeTitle = () => gameTitle.textContent = 'Test';
     const sayHi = () => alert('Hello!!'); 
     const hiLog = () => console.log('This is a test log!')
+
+    sayHi();
+
     return { changeTitle, hiLog };
 })();
 
-gameBoard.changeTitle();
-gameBoard.sayHi();
-gameBoard.hiLog();
 
 //object factory for the players? (choose between 2 people playing or against computer)
 (function(){
