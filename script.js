0908e8026644833809422b1256fa8d7e5c9206f1
DirtyReboot <96993283+IsaacWild@users.noreply.github.com>
2022-04-25 12:34:35 +0100

    Module test worked

--- script.js
+++ script.js
@@ -2,28 +2,15 @@
 //This works!!
 const gameBoard = (() => {
     const gameTitle = document.querySelector('.title');
+
+    const render = 'make game board and insert into DOM';
+
     const changeTitle = () => gameTitle.textContent = 'Test';
     const sayHi = () => alert('Hello!!'); 
     const hiLog = () => console.log('This is a test log!')
 
-    sayHi();
 
     return { changeTitle, hiLog };
 })();
-
-
-//object factory for the players? (choose between 2 people playing or against computer)
-(function(){
-    let _players ={
-        
-    }
-})()
-
-//Object to control flow of the game?
-(function(){
-    let _gamePlay ={
-        
-    }
-})()
 
 console.log('test');
