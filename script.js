bdeeff0a57b6d06d7baf4ca44616d71e8291312f
DirtyReboot <96993283+IsaacWild@users.noreply.github.com>
2022-04-25 12:04:29 +0100

    Testing implemantation of Factory Function

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+//Object factory for the gameboard logic?
+
+const gameTitle = document.querySelector('.title');
+
+const gameBoard = () => {
+    const changeTitle = () => gameTitle.textContent = 'Test';
+    const sayHi = () => alert('Hello!!'); 
+    const hiLog = () => console.log('This is a test log!')
+    return { changeTitle, hiLog };
+};
+
+gameBoard.changeTitle();
+gameBoard.sayHi();
+gameBoard.hiLog();
+
+//object factory for the players? (choose between 2 people playing or against computer)
 (function(){
-    let _gameBoard ={
+    let _players ={
         
     }
-    
+})()
 
+//Object to control flow of the game?
+(function(){
+    let _gamePlay ={
+        
+    }
 })()
