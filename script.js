549737a0e05e22f5a1357a56e4bcc8e0b4e17478
DirtyReboot <96993283+IsaacWild@users.noreply.github.com>
2022-04-27 08:15:31 +0100

    corrected use of logic && operators

--- script.js
+++ script.js
@@ -72,80 +72,80 @@ const checkWin = (gamePlay) => {
     }
     if (playString.length == 9){
         return console.log('tie')
-    }else if (a && b && c === 'x'){
+    }else if (a == 'x' && b == 'x' && c == 'x'){
         console.log(a,b,c)
         return console.log("X win's")
-    } else if (a && b && c === 'o'){
+    } else if (a == 'o' && b == 'o' && c == 'o'){
         console.log(a,b,c)
         return console.log("O win's")
     }else
     a = gamePlay[3]
     b = gamePlay[4]
     c = gamePlay[5]
-    if (a && b && c === 'x'){
+    if (a == 'x' && b == 'x' && c == 'x'){
         console.log(a,b,c)
         return console.log("X win's")
-    } else if (a && b && c === 'o'){
+    } else if (a == 'o' && b == 'o' && c == 'o'){
         console.log(a,b,c)
         return console.log("O win's")
     }else
     a = gamePlay[6]
     b = gamePlay[7]
     c = gamePlay[8]
-    if (a && b && c === 'x'){
+    if (a == 'x' && b == 'x' && c == 'x'){
         console.log(a,b,c)
         return console.log("X win's")
-    } else if (a && b && c === 'o'){
+    } else if (a == 'o' && b == 'o' && c == 'o'){
         console.log(a,b,c)
         return console.log("O win's")
     }else
     a = gamePlay[0]
     b = gamePlay[3]
     c = gamePlay[6]
-    if (a && b && c === 'x'){
+    if (a == 'x' && b == 'x' && c == 'x'){
         console.log(a,b,c)
         return console.log("X win's")
-    } else if (a && b && c === 'o'){
+    } else if (a == 'o' && b == 'o' && c == 'o'){
         console.log(a,b,c)
         return console.log("O win's")
     }else
     a = gamePlay[1]
     b = gamePlay[4]
     c = gamePlay[7]
-    if (a && b && c === 'x'){
+    if (a == 'x' && b == 'x' && c == 'x'){
         console.log(a,b,c)
         return console.log("X win's")
-    } else if (a && b && c === 'o'){
+    } else if (a == 'o' && b == 'o' && c == 'o'){
         console.log(a,b,c)
         return console.log("O win's")
     }else
     a = gamePlay[2]
     b = gamePlay[5]
     c = gamePlay[8]
-    if (a && b && c === 'x'){
+    if (a == 'x' && b == 'x' && c == 'x'){
         console.log(a,b,c)
         return console.log("X win's")
-    } else if (a && b && c === 'o'){
+    } else if (a == 'o' && b == 'o' && c == 'o'){
         console.log(a,b,c)
         return console.log("O win's")
     }else
     a = gamePlay[0]
     b = gamePlay[4]
     c = gamePlay[8]
-    if (a && b && c === 'x'){
+    if (a == 'x' && b == 'x' && c == 'x'){
         console.log(a,b,c)
         return console.log("X win's")
-    } else if (a && b && c === 'o'){
+    } else if (a == 'o' && b == 'o' && c == 'o'){
         console.log(a,b,c)
         return console.log("O win's")
     }else
     a = gamePlay[2]
     b = gamePlay[4]
     c = gamePlay[6]
-    if (a && b && c === 'x'){
+    if (a == 'x' && b == 'x' && c == 'x'){
         console.log(a,b,c)
         return console.log("X win's")
-    } else if (a && b && c === 'o'){
+    } else if (a == 'o' && b == 'o' && c === 'o'){
         console.log(a,b,c)
         return console.log("O win's")
     }  
