7ccaab68711106ece166f199302888c543cc6123
Isaac <isaacwildsecond@gmail.com>
2022-05-02 15:37:38 +0100

    Initil grid and render function working with styling

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let gamePlay = ['x','o','o','x','x','o','o','','x'];
+let currentPlayer = '';
+let player1Name = '';
+let player2Name = '';
+
 function render () {
-    
+    const gameGrid = document.querySelector('.gridWrapper');
+    for (let i = 0; i < gamePlay.length; i++) {
+        const gridBox = document.createElement('div')
+            gridBox.classList.add('gridBox');
+            gridBox.textContent = gamePlay[i]
+            gameGrid.appendChild(gridBox);
+    }
 }
+
+render()
